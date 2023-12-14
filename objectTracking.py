8e9badf14fd371d5ca061f235c3bc0fa6023850d
mshahnaza <150419889+mshahnaza@users.noreply.github.com>
2023-12-14 21:15:55 +0600

    finished code

---
+++ objectTracking.py
@@ -0,0 +1,54 @@
+import cv2
+from tracker import *
+
+# Create tracker object
+tracker = EuclideanDistTracker()
+
+cap = cv2.VideoCapture("highway.mp4")
+
+# Object detection from Stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+# Initialize total count and set for unique IDs
+total_count = 0
+unique_ids = set()
+
+while True:
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+
+    # Extract Region of interest
+    roi = frame[340: 720, 500: 800]
+
+    # 1. Object Detection
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+        # Calculate area and remove small elements
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(cnt)
+            detections.append([x, y, w, h])
+
+    # 2. Object Tracking
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, obj_id = box_id
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+        # Check if the object ID is already counted
+        if obj_id not in unique_ids:
+            total_count += 1
+            unique_ids.add(obj_id)
+
+    # Display total count at the top of the frame in green color
+    cv2.putText(frame, f'Total Count: {total_count}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+cap.release()
+cv2.destroyAllWindows()
